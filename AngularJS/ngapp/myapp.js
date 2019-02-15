35a8f76fd803a0f949638bc5a3e21d283e4e2b10
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2019-02-15 22:27:23 +0100

    ng-filter, ng-include

--- AngularJS/ngapp/myapp.js
+++ AngularJS/ngapp/myapp.js
@@ -7,9 +7,24 @@ MyApp.run(function(){});
 
 
 MyApp.controller("MojController", function($scope){
-
+    // Dane do ASJZad03
     $scope.wiadomosc = "Moje kolory";
 
     $scope.kolory = ["red", "black", "green", "blue", "grey"];
 
+    // dane do ASJZad04
+    $scope.tytul = "Lista gości";
+
+    $scope.lista = [
+        {imie: "Tomek", miejsce: "A1", wiek: 25},
+        {imie: "Agnieszka", miejsce: "A2", wiek: 20},
+        {imie: "Marcin", miejsce: "B1", wiek: 18},
+        {imie: "Monika", miejsce: "B2", wiek: 22},
+        {imie: "Łukasz", miejsce: "C1", wiek: 84},
+        {imie: "Kasia", miejsce: "C2", wiek: 75},
+        {imie: "Franek", miejsce: "D1", wiek: 38},
+        {imie: "Małgosia", miejsce: "D2", wiek: 44},
+    ];
+
 });
+
