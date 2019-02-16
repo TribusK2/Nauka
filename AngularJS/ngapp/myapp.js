2b63523808a9c96f5b08ec5d6891100cce58d1f9
Krzysztof Rytlewski <krzysztof.rytlewski@o2.pl>
2019-02-16 10:11:55 +0100

    ng-hide, ng-ckick

--- AngularJS/ngapp/myapp.js
+++ AngularJS/ngapp/myapp.js
@@ -12,19 +12,24 @@ MyApp.controller("MojController", function($scope){
 
     $scope.kolory = ["red", "black", "green", "blue", "grey"];
 
-    // dane do ASJZad04
+    // dane do ASJZad04 (ASJZad05)  
     $scope.tytul = "Lista gości";
 
     $scope.lista = [
-        {imie: "Tomek", miejsce: "A1", wiek: 25},
-        {imie: "Agnieszka", miejsce: "A2", wiek: 20},
-        {imie: "Marcin", miejsce: "B1", wiek: 18},
-        {imie: "Monika", miejsce: "B2", wiek: 22},
-        {imie: "Łukasz", miejsce: "C1", wiek: 84},
-        {imie: "Kasia", miejsce: "C2", wiek: 75},
-        {imie: "Franek", miejsce: "D1", wiek: 38},
-        {imie: "Małgosia", miejsce: "D2", wiek: 44},
+        {imie: "Tomek", miejsce: "A1", wiek: 25, dostepnosc: true},
+        {imie: "Agnieszka", miejsce: "A2", wiek: 20, dostepnosc: true},
+        {imie: "Marcin", miejsce: "B1", wiek: 18, dostepnosc: true},
+        {imie: "Monika", miejsce: "B2", wiek: 22, dostepnosc: true},
+        {imie: "Łukasz", miejsce: "C1", wiek: 84, dostepnosc: true},
+        {imie: "Kasia", miejsce: "C2", wiek: 75, dostepnosc: true},
+        {imie: "Franek", miejsce: "D1", wiek: 38, dostepnosc: true},
+        {imie: "Małgosia", miejsce: "D2", wiek: 44, dostepnosc: true},
     ];
 
+    $scope.reset = function(){
+        for (i=0; i<$scope.lista.length; i++){
+        $scope.lista[i].dostepnosc = true
+        };   
+    };
 });
 
